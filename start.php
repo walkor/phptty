678f65976e26fa948b28d04c8c0a3c8cd1d2f9c2
walkor <walkor@workerman.net>
2016-10-31 22:37:37 +0800

    composer

--- start.php
+++ start.php
@@ -25,7 +25,8 @@ define('ALLOW_CLIENT_INPUT', true);
 // Uinix user for command. Recommend nobody www etc. 
 define('USER', 'www-data');
 
-require_once __DIR__ . '/Workerman/Autoloader.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
 $worker = new Worker("Websocket://0.0.0.0:7778");
 $worker->name = 'phptty';
 $worker->user = USER;
