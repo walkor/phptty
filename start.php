1904fd9e81470a4f6de195373df92a85d8f0743c
walkor <walkor@users.noreply.github.com>
2020-03-09 19:28:25 +0800

    Update start.php

--- start.php
+++ start.php
@@ -13,8 +13,9 @@
 */
 
 use \Workerman\Worker;
-use \Workerman\WebServer;
-use \Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+use Workerman\Connection\TcpConnection;
 
 // Command. For example 'tail -f /var/log/nginx/access.log'.
 define('CMD', 'htop');
@@ -98,8 +99,54 @@ $worker->onWorkerStop = function($worker)
     }
 };
 
-$webserver = new WebServer('http://0.0.0.0:7779');
-$webserver->addRoot('localhost', __DIR__ . '/Web');
+$web = new Worker('http://0.0.0.0:7779');
+$web->name = 'web';
+
+define('WEBROOT', __DIR__ . '/Web');
+
+$web->onMessage = function (TcpConnection $connection, Request $request) {
+    $path = $request->path();
+    if ($path === '/') {
+        $connection->send(exec_php_file(WEBROOT.'/index.html'));
+        return;
+    }
+    $file = realpath(WEBROOT. $path);
+    if (false === $file) {
+        $connection->send(new Response(404, array(), '<h3>404 Not Found</h3>'));
+        return;
+    }
+    // Security check! Very important!!!
+    if (strpos($file, WEBROOT) !== 0) {
+        $connection->send(new Response(400));
+        return;
+    }
+    if (\pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+        $connection->send(exec_php_file($file));
+        return;
+    }
+
+    if (!empty($if_modified_since = $request->header('if-modified-since'))) {
+        // Check 304.
+        $info = \stat($file);
+        $modified_time = $info ? \date('D, d M Y H:i:s', $info['mtime']) . ' ' . \date_default_timezone_get() : '';
+        if ($modified_time === $if_modified_since) {
+            $connection->send(new Response(304));
+            return;
+        }
+    }
+    $connection->send((new Response())->withFile($file));
+};
+
+function exec_php_file($file) {
+    \ob_start();
+    // Try to include php file.
+    try {
+        include $file;
+    } catch (\Exception $e) {
+        echo $e;
+    }
+    return \ob_get_clean();
+}
 
 if(!defined('GLOBAL_START'))
 {
